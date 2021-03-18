280debe1d51a008e911fdaca5cc252e6930c99bf
Ricardo Meireles <cadomeireles@gmail.com>
2021-03-18 12:49:09 -0300

    Add a SQL with initial test data

---
+++ misc/initial_data.sql
@@ -0,0 +1,10 @@
+-- CLIENTS
+INSERT INTO clients (id, name, cpf, phone) VALUES ('0cbc35ea-f702-4753-8ef7-7f8a8bc7e536', 'Adriano Imperador', '22222222222', '215555555555');
+INSERT INTO clients (id, name, cpf, phone) VALUES ('c20d15f8-fd97-4de5-8409-dc2fe9c581d6', 'Zico', '33333333333', '216666666666');
+INSERT INTO clients (id, name, cpf, phone) VALUES ('313ee2c9-4a6c-4480-92d2-bea32027d250', 'Petkovic', '11111111111', '219999999999');
+-- PRODUCTS
+INSERT INTO products (id, name, price, weight, volume) VALUES ('bdaeaaa1-c8fc-4407-bc39-450eb4afdc88', 'Farinha', 5.00, 1000.00, 6000.00);
+INSERT INTO products (id, name, price, weight, volume) VALUES ('6f7015a2-fe1a-427d-95ad-2b1a76a56fe7', 'Peixe', 20.00, 1000.00, 9000.00);
+INSERT INTO products (id, name, price, weight, volume) VALUES ('d347e2f5-a48f-4ada-b3a7-3de8043142e2', 'Macaxeira', 10.00, 1000.00, 9000.00);
+INSERT INTO products (id, name, price, weight, volume) VALUES ('f4126c15-b607-40bc-98b3-05719940dfd8', 'Cajú (caixa)', 8.00, 1000.00, 9000.00);
+INSERT INTO products (id, name, price, weight, volume) VALUES ('fb276f4a-7a66-4fb0-9bff-24b3365a11b0', 'Cajuína', 7.00, 500.00, 3000.00);
